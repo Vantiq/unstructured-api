7082366ccbe4f2e3c2cb5feb7f614e6bb3bcca90
Sean Fitts <sfitts@vantiq.com>
2024-08-19 14:39:19 -0700

    Add support for parallel URL downloads

--- prepline_general/api/vantiq_app.py
+++ prepline_general/api/vantiq_app.py
@@ -1,11 +1,14 @@
-import io
+import os
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+from tempfile import TemporaryDirectory, SpooledTemporaryFile
 from typing import List, Optional, Union
 
 import requests
 from fastapi import APIRouter, UploadFile
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from starlette.requests import Request
-from unstructured.file_utils.filetype import detect_filetype, FILETYPE_TO_MIMETYPE
+from unstructured.file_utils.filetype import detect_filetype, FILETYPE_TO_MIMETYPE, FileType
 
 from .app import app
 from .general import general_partition
@@ -47,10 +50,6 @@ class PartitionUrls(GeneralFormParams):
     overlap_all: bool = False
 
 
-class NamedIO(io.BytesIO):
-    name: str
-
-
 @vantiq_router.post(
     "/general/v0/urls",
     openapi_extra={"x-speakeasy-name-override": "partition_url"},
@@ -64,35 +63,74 @@ def partition_urls(
     request: Request,
     to_partition: PartitionUrls,
 ):
-    files: List[UploadFile] = []
-    for entry in to_partition.urls:
-        # Is this just a URL or the URL with a content type?
-        if isinstance(entry, UrlWithContext):
-            url = entry.url
-            content_type = entry.content_type
-            url_headers = entry.headers
-            filename = entry.filename or url
+    # Create a temp directory to store the files
+    temp_root = os.environ.get("UNSTRUCTURED_DOWNLOAD_DIR", None)
+    with TemporaryDirectory(dir=temp_root) as temp_dir:
+        # Curry processing function with the request and directory
+        download_func = partial(download_for_processing, request=request, dir_name=temp_dir)
+        thread_count = int(os.environ.get("UNSTRUCTURED_DOWNLOAD_THREADS", 2))
+
+        # Download the files (possibly concurrently)
+        files: List[UploadFile] = []
+        if len(to_partition.urls) == 1 or thread_count == 1:
+            for url in to_partition.urls:
+                files.append(download_func(url))
         else:
-            url = entry
-            content_type = None
-            url_headers = None
-            filename = url
+            with ThreadPoolExecutor(max_workers=thread_count) as executor:
+                for result in executor.map(download_func, to_partition.urls):
+                    files.append(result)
 
-        # Fetch the content from the URL and treat as named ByteIO object
-        response = requests.get(url, headers=url_headers)
-        file = NamedIO(response.content)
-        file.name = filename
+        # Partition the files
+        return general_partition(request, files, to_partition)
 
-        # Determine the file type from the content/name
+
+# noinspection PyAbstractClass
+class NameOverride(SpooledTemporaryFile):
+    name_override: str
+
+    @property
+    def name(self):
+        return self.name_override
+
+
+def download_for_processing(entry: Union[str, UrlWithContext], request: Request, dir_name: str) -> UploadFile:
+    # Is this just a URL or the URL with a content type?
+    if isinstance(entry, UrlWithContext):
+        url = entry.url
+        content_type = entry.content_type
+        url_headers = entry.headers
+        filename = entry.filename or url
+    else:
+        url = entry
+        content_type = None
+        url_headers = None
+        filename = url
+
+    # Fetch the content from the URL to a temp file
+    tmp_file = SpooledTemporaryFile(max_size=10 * 1024 * 1024, dir=dir_name)
+    response = requests.get(url, headers=url_headers, stream=True)
+    for chunk in response.iter_content(chunk_size=1024*1024):
+        tmp_file.write(chunk)
+    tmp_file.seek(0)
+
+    # Determine the file type from the content/name (if not provided)
+    if not content_type:
+        # Start by passing the filename to the detection function.
         encoding = response.headers.get("Content-Encoding", "utf-8")
-        filetype = detect_filetype(file=file, encoding=encoding, content_type=content_type)
+        filetype = detect_filetype(file=tmp_file, file_filename=filename, encoding=encoding)
+        if filetype is FileType.UNK:
+            # If the file type is still unknown, try again without the explicit filename, but first we need
+            # to monkey patch the temp file so that when the code asks for a name, it gets one (don't ask).
+            tmp_file.__class__ = NameOverride
+            tmp_file.name_override = filename
+            filetype = detect_filetype(file=tmp_file, encoding=encoding)
+        content_type = FILETYPE_TO_MIMETYPE[filetype]
 
-        # Construct an UploadFile object with the file and its metadata, so we can use the general_partition function
-        headers = request.headers.mutablecopy()
-        headers["Content-Type"] = content_type or FILETYPE_TO_MIMETYPE[filetype]
-        upload = UploadFile(file= file, filename=filename, headers=headers)
-        files.append(upload)
-    return general_partition(request, files, to_partition)
+    # Construct an UploadFile object with the file and its metadata, so we can use the general_partition function
+    headers = request.headers.mutablecopy()
+    headers["Content-Type"] = content_type
+    # noinspection PyTypeChecker
+    return UploadFile(file=tmp_file, filename=filename, headers=headers)
 
 
 app.include_router(vantiq_router)
